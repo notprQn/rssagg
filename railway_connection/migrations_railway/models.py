f17d93ab33c5a4100aec1a3ec73d0bfe892bad03
João Pedro Resende Silva <90474257+notprQn@users.noreply.github.com>
2024-01-30 13:09:59 -0300

    adding python migrations

---
+++ railway_connection/migrations_railway/models.py
@@ -0,0 +1,44 @@
+# nome_do_app/models.py
+from django.db import models
+import uuid
+from django.utils import timezone
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    updated_at = models.DateTimeField(default=timezone.now)
+    name = models.TextField()
+    api_key = models.CharField(max_length=64, unique=True, default='')
+
+    def save(self, *args, **kwargs):
+        self.updated_at = timezone.now()
+        return super().save(*args, **kwargs)
+
+class Feed(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    updated_at = models.DateTimeField(default=timezone.now)
+    name = models.TextField()
+    url = models.TextField(unique=True)
+    last_fetched_at = models.DateTimeField(null=True, blank=True)
+
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        self.updated_at = timezone.now()
+        return super().save(*args, **kwargs)
+    
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    updated_at = models.DateTimeField(default=timezone.now)
+    title = models.TextField()
+    url = models.TextField(unique=True)
+    description = models.TextField(blank=True, null=True)
+    published_at = models.DateTimeField(blank=True, null=True)
+    feed = models.ForeignKey('Feed', on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        self.updated_at = timezone.now()
+        return super().save(*args, **kwargs)
+
